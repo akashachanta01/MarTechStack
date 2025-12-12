67329e51b37fdd4aa9f48d502da740bd49a94016
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-11 20:16:29 -0500

    Update forms.py

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Job
+from .models import Job, Subscriber
 
 class JobSubmissionForm(forms.ModelForm):
     class Meta:
@@ -15,3 +15,9 @@ class JobSubmissionForm(forms.ModelForm):
             'tags': forms.TextInput(attrs={'class': 'w-full px-4 py-3 rounded-lg border border-slate-300 focus:ring-2 focus:ring-teal-500 outline-none', 'placeholder': 'e.g. Marketo, Salesforce, SQL'}),
             'description': forms.Textarea(attrs={'class': 'w-full px-4 py-3 rounded-lg border border-slate-300 focus:ring-2 focus:ring-teal-500 outline-none h-40', 'placeholder': 'Paste job description here...'}),
         }
+
+# --- NEW SUBSCRIBER FORM ---
+class SubscriberForm(forms.ModelForm):
+    class Meta:
+        model = Subscriber
+        fields = ['email']
