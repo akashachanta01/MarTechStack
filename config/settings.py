bba51dbe304b9675fbfcc77a1796617036aa97da
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-11 20:48:13 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -154,3 +154,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ==============================================
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# ==============================================
+# EMAIL CONFIGURATION (SECURE)
+# ==============================================
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# These read from the Render Environment (Safe!)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = f'MarTechStack <{EMAIL_HOST_USER}>'
