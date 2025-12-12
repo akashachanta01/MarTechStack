2c6bf91894359d3fc1cc562672b10d1d30997086
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-11 19:31:32 -0500

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.job_list, name='job_list'),
     path('job/<int:pk>/', views.job_detail, name='job_detail'),
+    path('post-job/', views.post_job, name='post_job'), # New!
+    # path('import/', views.import_job, name='import_job'), # (Keep this if you used the previous step)
 ]
