d6baafc1e2211ec5d766b32b350b8991074a6820
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-16 12:36:53 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,9 @@ pip install -r requirements.txt
 # Convert static files
 python manage.py collectstatic --no-input
 
+# ⚠️ GITHUB-EDIT HACK: Create migrations on the server since we don't do it locally
+# Only keep this while you are actively changing DB models.
+python manage.py makemigrations
+
 # Run database migrations
 python manage.py migrate
