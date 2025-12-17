cd6cf83247a92c5895ecbe14a90a9ab3147be700
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-16 20:17:37 -0500

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.job_list, name='job_list'),
-    path('job/<int:job_id>/', views.job_detail, name='job_detail'),
     
     # New Post Job Routes
     path('post-job/', views.post_job, name='post_job'),
