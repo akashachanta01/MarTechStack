bc07690691541158a90cfcff76ea6d64c5220a53
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-17 16:51:40 -0500

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,14 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.job_list, name='job_list'),
-    
-    # Post Job Routes
     path('post-job/', views.post_job, name='post_job'),
     path('post-job/success/', views.post_job_success, name='post_job_success'),
     
-    path('subscribe/', views.subscribe, name='subscribe'),
+    # NEW: Stripe Webhook
+    path('webhook/stripe/', views.stripe_webhook, name='stripe_webhook'),
     
-    # Review Queue (Admin)
+    path('subscribe/', views.subscribe, name='subscribe'),
     path('staff/review/', views.review_queue, name='review_queue'),
     path('staff/review/<int:job_id>/<str:action>/', views.review_action, name='review_action'),
 ]
