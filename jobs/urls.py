80e0b158c1146008a6ba27c0cae6f4e878a6a69d
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-22 23:43:30 -0500

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,29 +4,32 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.http import HttpResponse
+
+# Import the Sitemap class
 from jobs.sitemaps import JobSitemap
 
-# Define Sitemaps
+# Define the dictionary of sitemaps
 sitemaps = {
     'jobs': JobSitemap,
 }
 
-# Define Robots.txt
+# Simple Robots.txt view
 def robots_txt(request):
-    lines = [
-        "User-agent: *",
-        "Disallow: /admin/",
-        "Disallow: /staff/",
-        "",
-        f"Sitemap: {settings.DOMAIN_URL}/sitemap.xml",
-    ]
-    return HttpResponse("\n".join(lines), content_type="text/plain")
+    content = f"""User-agent: *
+Disallow: /admin/
+Disallow: /staff/
+
+Sitemap: {settings.DOMAIN_URL}/sitemap.xml
+"""
+    return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Main App URLs
     path('', include('jobs.urls')),
     
-    # --- SEO ENDPOINTS ---
+    # SEO: Sitemap & Robots
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', robots_txt),
 ]
