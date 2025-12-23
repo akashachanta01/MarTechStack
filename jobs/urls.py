3ef2edd3f70e18440a5d481f3d99d4869b151074
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2025-12-22 23:36:28 -0500

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,20 +1,36 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.http import HttpResponse
+from jobs.sitemaps import JobSitemap
+
+# Define Sitemaps
+sitemaps = {
+    'jobs': JobSitemap,
+}
+
+# Define Robots.txt
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Disallow: /admin/",
+        "Disallow: /staff/",
+        "",
+        f"Sitemap: {settings.DOMAIN_URL}/sitemap.xml",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
 
 urlpatterns = [
-    path('', views.job_list, name='job_list'),
+    path('admin/', admin.site.urls),
+    path('', include('jobs.urls')),
     
-    # --- NEW SEO DETAIL PAGE ---
-    # This captures /job/123/marketing-manager-at-hubspot/
-    path('job/<int:id>/<slug:slug>/', views.job_detail, name='job_detail'),
-
-    path('post-job/', views.post_job, name='post_job'),
-    path('post-job/success/', views.post_job_success, name='post_job_success'),
-    
-    # NEW: Stripe Webhook
-    path('webhook/stripe/', views.stripe_webhook, name='stripe_webhook'),
-    
-    path('subscribe/', views.subscribe, name='subscribe'),
-    path('staff/review/', views.review_queue, name='review_queue'),
-    path('staff/review/<int:job_id>/<str:action>/', views.review_action, name='review_action'),
+    # --- SEO ENDPOINTS ---
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', robots_txt),
 ]
+
+# Serve media files (images) during development/debug
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
