c9350679aebb8cd70a192816524a2b1c6e0f0ae8
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2026-01-03 14:51:36 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,41 +5,13 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps import Sitemap
 from django.contrib.sitemaps.views import sitemap
 from django.http import HttpResponse
-
-# --- 1. JOB SITEMAP ---
-class JobSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.8
-    protocol = 'https'
-
-    def items(self):
-        from jobs.models import Job
-        return Job.objects.filter(is_active=True, screening_status='approved')
-
-    def lastmod(self, obj):
-        return obj.updated_at
-
-    def location(self, obj):
-        return reverse('job_detail', args=[obj.id, obj.slug])
-
-# --- 2. TOOL SITEMAP (New Topic Clusters) ---
-class ToolSitemap(Sitemap):
-    changefreq = "weekly"
-    priority = 0.6
-    protocol = 'https'
-
-    def items(self):
-        from jobs.models import Tool
-        # Only list tools that actually have jobs
-        return Tool.objects.filter(jobs__is_active=True).distinct()
-
-    def location(self, obj):
-        return reverse('tool_detail', args=[obj.slug])
+from jobs.sitemaps import JobSitemap, ToolSitemap, SEOLandingSitemap # <--- Added Import
 
 # --- DEFINITIONS ---
 sitemaps = {
     'jobs': JobSitemap,
     'tools': ToolSitemap, 
+    'seo_landing': SEOLandingSitemap, # <--- Added
 }
 
 # --- ROBOTS.TXT ---
