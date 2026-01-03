bbc20882f2c39570471639b9475d19f8b0d00d0d
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2026-01-03 17:13:02 -0500

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from .feeds import LatestJobsFeed
 
 urlpatterns = [
     path('', views.job_list, name='job_list'),
     
+    # --- GROWTH ENGINE ---
+    path('salary-guide/', views.salary_guide, name='salary_guide'),
+    path('feed/', LatestJobsFeed(), name='job_feed'),
+
     # --- STATIC PAGES ---
     path('about/', views.about, name='about'),
     path('for-employers/', views.for_employers, name='for_employers'),
@@ -17,9 +22,7 @@ urlpatterns = [
     path('post-job/', views.post_job, name='post_job'),
     path('post-job/success/', views.post_job_success, name='post_job_success'),
     
-    # Stripe Webhook
     path('webhook/stripe/', views.stripe_webhook, name='stripe_webhook'),
-    
     path('subscribe/', views.subscribe, name='subscribe'),
     path('unsubscribe/', views.unsubscribe, name='unsubscribe'),
 
@@ -27,9 +30,6 @@ urlpatterns = [
     path('staff/review/<int:job_id>/<str:action>/', views.review_action, name='review_action'),
 
     # --- PROGRAMMATIC SEO (Must be last) ---
-    # Matches: /remote/hubspot-jobs/ or /new-york/salesforce-jobs/
     path('<str:location_slug>/<slug:tool_slug>-jobs/', views.seo_landing_page, name='seo_tool_loc'),
-    
-    # Matches: /remote/jobs/ or /london/jobs/
     path('<str:location_slug>/jobs/', views.seo_landing_page, name='seo_loc_only'),
 ]
