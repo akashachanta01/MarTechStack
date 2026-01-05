6a457f2831ebcd22418b99cd8313e028a06dd82f
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2026-01-05 14:55:45 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,36 +1,46 @@
 from django.contrib import admin
-from django.urls import path, include, reverse
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.http import HttpResponse
-from jobs.sitemaps import JobSitemap, ToolSitemap, SEOLandingSitemap, StaticViewSitemap # <--- Updated Import
 
-# --- DEFINITIONS ---
+# Import your Sitemap logic
+from jobs.sitemaps import JobSitemap, ToolSitemap, SEOLandingSitemap, StaticViewSitemap
+
+# --- 1. DEFINE SITEMAPS ---
+# This tells Django how to build the map for Google
 sitemaps = {
     'jobs': JobSitemap,
-    'tools': ToolSitemap, 
+    'tools': ToolSitemap,
     'seo_landing': SEOLandingSitemap,
-    'static': StaticViewSitemap, # <--- Added
+    'static': StaticViewSitemap,
 }
 
-# --- ROBOTS.TXT ---
+# --- 2. ROBOTS.TXT VIEW ---
+# This serves the instructions for Googlebot
 def robots_txt(request):
-    content = f"""User-agent: *
+    # We hardcode the production domain to be 100% safe
+    content = """User-agent: *
 Disallow: /admin/
 Disallow: /staff/
 Disallow: /webhook/
+Disallow: /post-job/success/
 
-Sitemap: {settings.DOMAIN_URL}/sitemap.xml
+Sitemap: https://martechjobs.io/sitemap.xml
 """
     return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
+    # Admin & Apps
     path('admin/', admin.site.urls),
     path('', include('jobs.urls')),
+
+    # SEO Paths
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', robots_txt),
 ]
 
+# Static media serving for debug mode
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
