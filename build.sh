c0ffeac18c7e946f0e80c63a4999a2ddc36a6585
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2026-01-05 14:41:38 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ python manage.py collectstatic --no-input
 # 3. Run standard migrations
 # Note: We removed 'makemigrations' because your DB is now in sync.
 python manage.py migrate
+
+# 4. FIX SEO DOMAIN (Critical for Sitemap.xml)
+# This ensures Django knows the site is 'martechjobs.io', not 'example.com'
+python manage.py fix_seo_domain
