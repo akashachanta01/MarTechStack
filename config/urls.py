c0cf2a2f71df1e57b0e3532b5be2c82486e64137
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2026-01-05 20:04:44 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,21 +6,21 @@ from django.contrib.sitemaps.views import sitemap
 from django.http import HttpResponse
 
 # Import your Sitemap logic
-from jobs.sitemaps import JobSitemap, ToolSitemap, SEOLandingSitemap, StaticViewSitemap
+from jobs.sitemaps import JobSitemap, ToolSitemap, SEOLandingSitemap, StaticViewSitemap, BlogSitemap
 
 # --- 1. DEFINE SITEMAPS ---
-# This tells Django how to build the map for Google
+# This tells Django (and Google) how to map your content
 sitemaps = {
     'jobs': JobSitemap,
     'tools': ToolSitemap,
     'seo_landing': SEOLandingSitemap,
     'static': StaticViewSitemap,
+    'blog': BlogSitemap,  # <-- This adds your new blog posts to the sitemap
 }
 
 # --- 2. ROBOTS.TXT VIEW ---
 # This serves the instructions for Googlebot
 def robots_txt(request):
-    # We hardcode the production domain to be 100% safe
     content = """User-agent: *
 Disallow: /admin/
 Disallow: /staff/
@@ -34,13 +34,13 @@ Sitemap: https://martechjobs.io/sitemap.xml
 urlpatterns = [
     # Admin & Apps
     path('admin/', admin.site.urls),
-    path('', include('jobs.urls')),
+    path('', include('jobs.urls')),  # Delegates normal pages to your jobs app
 
     # SEO Paths
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', robots_txt),
 ]
 
-# Static media serving for debug mode
+# Static media serving for debug mode (Local development)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
