a44d2e8d2313334b7d303fb385970c8546f946e1
akashachanta01 <39563268+akashachanta01@users.noreply.github.com>
2026-04-09 21:26:06 -0400

    Update emails.py

--- jobs/emails.py
+++ jobs/emails.py
@@ -89,7 +89,7 @@ def send_admin_new_subscriber_alert(subscriber_email, user_agent, ip_address):
     def _send():
         try:
             # UPDATED: Admin Email fallback
-            admin_email = getattr(settings, 'EMAIL_HOST_USER', 'hello@martechstack.io')
+            admin_email = getattr(settings, 'EMAIL_HOST_USER', 'martechjobs@gmail.com')
             subject = f"🔔 New Subscriber: {subscriber_email}"
             body = f"""
 New subscriber: {subscriber_email}
